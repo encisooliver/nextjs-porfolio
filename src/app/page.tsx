76138af28ebe994de122ea481005403158cc8279
Oliver <oliverenciso07@gmail.com>
2025-01-16 06:39:57 +0800

    initial update page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@ import Link from 'next/link'
 
 export default function Home() {
   return  ( 
-    <main className="flex flex-col items-center justify-center min-h-screen bg-gray-50 text-gray-800">
-      <section className="grid grid-cols-1 md:grid-cols-2 gap-6 p-6">
+    <main >
+      {/* <section className="grid grid-cols-1 md:grid-cols-2 gap-6 p-6">
         <div className="text-center md:text-left">
           <h1 className="text-4xl font-bold mb-4">Oliver Enciso</h1>
-          <h2 className="text-2xl font-medium text-blue-600 mb-6">Software Developer | Web3 | DevOps | Full-stack</h2>
+          <h2 className="text-2xl font-medium text-blue-600 mb-6">Software Engineer | Web3 | DevOps | Full-stack</h2>
         </div>
         <div className="text-center md:text-left">
           <p className="text-lg leading-relaxed">
@@ -16,7 +16,30 @@ export default function Home() {
             objectives.
           </p>
         </div>
-      </section>
+      </section> */}
+      <section className="bg-gradient-to-r from-gray-500 via-gray-100 to-gray-400 text-black py-16">
+    <div className="container mx-auto px-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center h-full md:h-[50vh]">
+
+        <div className="flex flex-col justify-center h-full">
+          <h1 className="text-3xl md:text-4xl font-bold">
+            Oliver Enciso
+          </h1>
+          <p className="text-lg font-medium mt-2">
+            Software Engineer | Web3 | DevOps | Full-stack
+          </p>
+        </div>
+
+        <div className="flex flex-col justify-center h-full">
+          <p className="text-base md:text-lg leading-relaxed">
+            Software developer with five years of hands-on experience, specializing in transforming creative concepts into user-friendly applications. Passionate about coding, committed to problem-solving, and possessing a keen sense of unity in guiding teams towards their objectives.
+          </p>
+        </div>
+
+      </div>
+    </div>
+  </section>
+ 
     </main>
   )
 }
--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Skills() {
   return (
-    <div className="min-h-screen bg-gray-100 py-10">
+    <div className="min-h-screen bg-white py-10">
       <div className="container mx-auto px-4">
         <h1 className="text-4xl font-bold text-center text-gray-800 mb-6">Skills</h1>
         <div className="bg-white shadow-md rounded-lg p-6">
